793c2dbfea6a00399d8fa3da22ee7ff7faa1d78f
AnnVinogradova <saikotakaki@gmail.com>
2023-05-24 17:01:33 +0300

    +++

--- redux/actions.js
+++ redux/actions.js
@@ -4,14 +4,17 @@ export const DELETE_TODO  = 'DELETE_TODO';
 export const TOGGLE_TODO  = 'TOGGLE_TODO';
 
 
-export function addTask(delta){
-  return {type: ADD_TODO, delta }
-}
+export const addTodo = (text) => ({
+  type: ADD_TODO,
+  payload: { text }
+});
 
-export function deleteTask(delta){
-  return {type: DELETE_TODO, delta }
-}
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  payload: { id }
+});
 
-export function toggleTask(delta){
-  return {type: TOGGLE_TODO, delta }
-}
+export const deleteTodo = (id) => ({
+  type: DELETE_TODO,
+  payload: { id }
+});
